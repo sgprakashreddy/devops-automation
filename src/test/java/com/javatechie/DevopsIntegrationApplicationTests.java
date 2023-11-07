bb5ac1173b3f3b6d3f487651ef45aefb0848e4d8
reddysgp <reddysgp@gmail.com>
2023-11-07 15:55:28 +0100

    explict failure conditions

--- src/test/java/com/javatechie/DevopsIntegrationApplicationTests.java
+++ src/test/java/com/javatechie/DevopsIntegrationApplicationTests.java
@@ -1,13 +1,19 @@
 package com.javatechie;
 
 import org.junit.jupiter.api.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.test.context.SpringBootTest;
+
 
 @SpringBootTest
 class DevopsIntegrationApplicationTests {
 
+    public static Logger logger= LoggerFactory.getLogger(DevopsIntegrationApplication.class);
+
     @Test
-    void contextLoads() {
+    contextLoads() {
+        logger.info("******** Test cases are executing  ******");
     }
 
 }
