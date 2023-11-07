dd562d6b12c03fa910f587b3a739cee2b4b27e5c
reddysgp <reddysgp@gmail.com>
2023-11-07 15:45:31 +0100

    updated loggers

--- src/main/java/com/javatechie/DevopsIntegrationApplication.java
+++ src/main/java/com/javatechie/DevopsIntegrationApplication.java
@@ -1,12 +1,23 @@
 package com.javatechie;
 
+import jakarta.annotation.PostConstruct;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class DevopsIntegrationApplication {
 
+    public static Logger logger= LoggerFactory.getLogger(DevopsIntegrationApplication.class);
+
+    @PostConstruct
+    public void init(){
+        logger.info("********* DevopsIntegrationApplication Started ********* ");
+    }
+
     public static void main(String[] args) {
+        logger.info("********* DevopsIntegrationApplication Executed ********* ");
         SpringApplication.run(DevopsIntegrationApplication.class, args);
     }
 
